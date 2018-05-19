7cada343ad4c51883280fdc71c2f0c7dbde97911
sontolesquad <39442325+sontolesquad@users.noreply.github.com>
2018-05-20 01:01:43 +0700

    Update subdo.sh

--- data/subdo.sh
+++ data/subdo.sh
@@ -6,12 +6,12 @@ echo '    |  ___|_ _| | _____    |_   _|__   ___ | |___ ';
 echo '    | |_ / _` | |/ / _ \_____| |/ _ \ / _ \| / __|';
 echo '    |  _| (_| |   <  __/_____| | (_) | (_) | \__ \';
 echo '    |_|  \__,_|_|\_\___|     |_|\___/ \___/|_|___/ v.1.0';
-echo ' ---------------- Coded By Cy#b3r00T ---------------';
+echo ' ---------------- Coded By Mrsontolex / Muhammad Zainul Ramadhan ---------------';
 echo '';
 read -p "Domain : http://" domain
 read -p "Delay? (1-5 ) :" delay
 read -p "Jumlah? (1-10000):" jumlah
-read -p "Output (test.txt) :" output
+read -p "Output (sctkontol.txt) :" output
 echo "[+] Generating..."
 count=1
 while [ $count -le $jumlah ]
